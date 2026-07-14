32ad2da8b8ff6788429dae23cef4eb019db7c910
Vasco Silvério <vascosilverio@unknown14109fda3a5b.lan>
2026-07-15 00:09:30 +0100

    Alteração data

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
 //-------------------- typed js  --------------------------------
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Web Designer', 'Software Developer'],
+    strings: ['Data Analyst', 'Information Management', 'Information Architect'],
     typeSpeed: 70,
     backSpeed:70,
     backDelay: 1000, 
